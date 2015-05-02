74ca3ed3f5992325f73631bfcf0bcabe8eb2c35f
Finjon Kiang <kiange@gmail.com>
2015-05-02 17:47:06 +0800

    update requests

--- query.php
+++ query.php
@@ -26,20 +26,23 @@ $options = array(
 $context = stream_context_create($options);
 $result = file_get_contents($url, false, $context);
 
+$basePath = __DIR__ . "/requests";
+
 $xml = simplexml_load_string($result, 'SimpleXMLElement', LIBXML_NOCDATA);
 foreach ($xml->records->record AS $record) {
     $timeStamp = strtotime($record->requested_datetime);
-    $d = date('Ymd', $timeStamp);
-    if (!file_exists(__DIR__ . '/' . $d)) {
-        mkdir(__DIR__ . '/' . $d, 0777);
+    $d = date('Y/m/d', $timeStamp);
+    $requestPath = "{$basePath}/{$d}";
+    if (!file_exists($requestPath)) {
+        mkdir($requestPath, 0777, true);
     }
     if (!empty($record->Pictures)) {
         $picCount = 0;
         foreach ($record->Pictures AS $pic) {
             ++$picCount;
-            file_put_contents(__DIR__ . "/{$d}/{$record->service_request_id}-{$picCount}.jpg", base64_decode((string) $pic->Picture->file));
+            file_put_contents("{$requestPath}/{$record->service_request_id}-{$picCount}.jpg", base64_decode((string) $pic->Picture->file));
         }
         unset($record->Pictures);
     }
-    file_put_contents(__DIR__ . "/{$d}/{$record->service_request_id}.json", json_encode($record, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+    file_put_contents("{$requestPath}/{$record->service_request_id}.json", json_encode($record, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
 }
