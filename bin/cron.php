c090fe6a2e2d846f5fb2c7da1893b83a8b4bd69a
auto commit <noreply@localhost>
2015-05-06 17:13:27 +0800

    update datasets @ 2015-05-06 17:12:25

--- bin/cron.php
+++ bin/cron.php
@@ -6,6 +6,6 @@ exec('/usr/bin/php -q /home/kiang/public_html/tainan_1999/query.php');
 
 exec('cd /home/kiang/public_html/tainan_1999 && /usr/bin/git add -A');
 
-exec("cd /home/kiang/public_html/tainan_1999 && /usr/bin/git --author 'auto commit <noreply@localhost>' -m 'update datasets @ {$now}'");
+exec("cd /home/kiang/public_html/tainan_1999 && /usr/bin/git commit --author 'auto commit <noreply@localhost>' -m 'update datasets @ {$now}'");
 
 exec('cd /home/kiang/public_html/tainan_1999 && /usr/bin/git push origin master');
