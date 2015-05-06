385a2407a69b5305dcadab9c80001e689b2e3fd8
Finjon Kiang <kiange@gmail.com>
2015-05-06 17:23:55 +0800

    use detected path in script

--- bin/cron.php
+++ bin/cron.php
@@ -1,11 +1,14 @@
 <?php
 
+$path = dirname(__DIR__);
 $now = date('Y-m-d H:i:s');
 
-exec('/usr/bin/php -q /home/kiang/public_html/tainan_1999/query.php');
+exec("/usr/bin/php -q {$path}/query.php");
 
-exec('cd /home/kiang/public_html/tainan_1999 && /usr/bin/git add -A');
+exec("cd {$path} && /usr/bin/git pull");
 
-exec("cd /home/kiang/public_html/tainan_1999 && /usr/bin/git commit --author 'auto commit <noreply@localhost>' -m 'update datasets @ {$now}'");
+exec("cd {$path} && /usr/bin/git add -A");
 
-exec('cd /home/kiang/public_html/tainan_1999 && /usr/bin/git push origin master');
+exec("cd {$path} && /usr/bin/git commit --author 'auto commit <noreply@localhost>' -m 'update datasets @ {$now}'");
+
+exec("cd {$path} && /usr/bin/git push origin master");
